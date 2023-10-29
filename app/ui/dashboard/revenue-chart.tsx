69457c67218d9ea2e6eb21c3728768c897116e48
babually <mmrishoally@ymail.com>
2023-10-29 18:22:41 +0300

    fixs bug about font

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,6 +1,6 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { poppins } from '@/app/ui/fonts';
 import { Revenue } from '@/app/lib/definitions';
 
 // This component is representational only.
@@ -25,7 +25,7 @@ export default async function RevenueChart({
 
   return (
     <div className="w-full md:col-span-4">
-      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+      <h2 className={`${poppins.className} mb-4 text-xl md:text-2xl`}>
         Recent Revenue
       </h2>
       {/* NOTE: comment in this code when you get to this point in the course */}
